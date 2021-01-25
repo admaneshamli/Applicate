a527a4905da4fc85778638a4886b767c72d34b67
admaneshamli <admaneshamli11@gmail.com>
2021-01-25 17:48:30 +0530

    Applicate

---
+++ src/main/java/com/applicate/ApplicateApplication.java
@@ -0,0 +1,13 @@
+package com.applicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applicate/ApplicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
